95565d400c57c778043f6f440788e713cd0ad390
sp1607 <77290138+sp1607@users.noreply.github.com>
2021-01-29 17:45:13 +0300

    Mod Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int price_ticket = 10_000;
-        int miles = price_ticket / 20;
+        int priceTicket = 10_000;
+        int miles = priceTicket / 20;
         System.out.println ("Bonus miles =" + miles);
     }
 }
