aa5836b2aa60e82bdada4206bb9e8fc1cdd5f149
sp1607 <77290138+sp1607@users.noreply.github.com>
2021-01-29 16:20:55 +0300

    Added .gitignore

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_ticket = 10_000;
+        int miles = price_ticket / 20;
+        System.out.println ("Bonus miles =" + miles);
+    }
+}
